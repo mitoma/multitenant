c8e5a1d2482db275094c17662760ce50d4788be4
Mitoma Ryo <mutetheradio@gmail.com>
2011-09-03 11:42:02 +0900

    to rails 3.1

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Multitenant::Application.config.secret_token = '521e02411a5b06d4f699b320bf9fc7f10bbd7e5dbbca5db7cf6efbbbf44f01cb4d92c1ce6c755a36b3369d636a0032752cf41b67eec8db523a040696fa40dbc8'
+Multitenant::Application.config.secret_token = '9a83820b154c518642c75e236899fd7895018e869c5958d80caf5fac0d9c3f2085b2c906dfef28f78bfa5f3c2936cde95b19620f28fdb976634c81a6fbf61f0d'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Multitenant::Application.config.session_store :cookie_store, :key => '_multitenant_session'
+Multitenant::Application.config.session_store :cookie_store, key: '_multitenant_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
